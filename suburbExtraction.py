070122c8b621f48f6299a4d710ef077d9a774999
JuanLu <juanlu@JuanLus-MacBook-Pro.local>
2016-01-21 16:51:43 +1100

    add main setence

--- suburbExtraction.py
+++ suburbExtraction.py
@@ -16,101 +16,107 @@ import sys
 # PRICECOL=4
 # DATECOL=16
 
-enquirySub="ASHFIELD"
-enquiryTable=sys.argv[1]
-print enquiryTable+'\n'  ###'../suburb_profiling/history_data/2016_01_20_lots.csv'
+def suburbExtraction(enquirySub, enquiryTable):
 
-filteredRow=[]
-bedroom1=[]
-bedroom2=[]
-bedroom3=[]
-priceBedroom1=[]
-priceBedroom2=[]
-priceBedroom3=[]
+	filteredRow=[]
+	bedroom1=[]
+	bedroom2=[]
+	bedroom3=[]
+	priceBedroom1=[]
+	priceBedroom2=[]
+	priceBedroom3=[]
 
-areaBedroom1=[]
-areaBedroom2=[]
-areaBedroom3=[]
-count=0
-with open(enquiryTable, 'rb') as csvfile:
-	#reader=csv.reader(csvfile)
-	reader=csv.DictReader(csvfile)
-	next(reader, None)
-	for row in reader:
-		# if count==0:	#the header of the table
-		# 	print row[0]
-		# 	newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
-		# 	filteredRow.append(newRow)
-		# 	count=count+1
-		projectName=row['Project']
-		destinateStr=enquirySub
-		po=projectName.find(destinateStr)
-		
-		if po>=0:
-			#newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
-			#filteredRow.append(newRow)
+	areaBedroom1=[]
+	areaBedroom2=[]
+	areaBedroom3=[]
+	count=0
+	with open(enquiryTable, 'rb') as csvfile:
+		#reader=csv.reader(csvfile)
+		reader=csv.DictReader(csvfile)
+		next(reader, None)
+		for row in reader:
+			# if count==0:	#the header of the table
+			# 	print row[0]
+			# 	newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
+			# 	filteredRow.append(newRow)
+			# 	count=count+1
+			projectName=row['Project']
+			destinateStr=enquirySub
+			po=projectName.find(destinateStr)
+			
+			if po>=0:
+				#newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
+				#filteredRow.append(newRow)
 
-			Bed1=["1","1+S"]
-			Bed2=["2", "2+S"]
-			Bed3=["3", "3+S"]	
+				Bed1=["1","1+S"]
+				Bed2=["2", "2+S"]
+				Bed3=["3", "3+S"]	
 
-			if row['Bedrooms'] in  Bed1: 
-				currentPrice=row['Price']
-				if len(currentPrice) <= 0:
-					continue
-				priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
-
-				# priceValue= currentPrice[1:len(currentPrice)]
-				# print ast.literal_eval(priceValue)
-				priceBedroom1.append(float(priceValue))
-
-				#calculate the area
-				currentArea=row['Internal Area']
-				areaBedroom1.append(float(currentArea))
-
-			if row['Bedrooms'] in  Bed2: 
-				currentPrice=row['Price']
-				if bool(currentPrice):
-					#print currentPrice
+				if row['Bedrooms'] in  Bed1: 
+					currentPrice=row['Price']
+					if len(currentPrice) <= 0:
+						continue
 					priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
 
-					priceBedroom2.append(float(priceValue))
+					# priceValue= currentPrice[1:len(currentPrice)]
+					# print ast.literal_eval(priceValue)
+					priceBedroom1.append(float(priceValue))
 
+					#calculate the area
 					currentArea=row['Internal Area']
-					areaBedroom2.append(float(currentArea))
+					areaBedroom1.append(float(currentArea))
+
+				if row['Bedrooms'] in  Bed2: 
+					currentPrice=row['Price']
+					if bool(currentPrice):
+						#print currentPrice
+						priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
+
+						priceBedroom2.append(float(priceValue))
+
+						currentArea=row['Internal Area']
+						areaBedroom2.append(float(currentArea))
 
 
-			if row['Bedrooms'] in  Bed3: 
-				currentPrice=row['Price']
-				if bool(currentPrice):
-					priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
-					priceBedroom3.append(float(priceValue))
-					currentArea=row['Internal Area']
-					areaBedroom3.append(float(currentArea))
+				if row['Bedrooms'] in  Bed3: 
+					currentPrice=row['Price']
+					if bool(currentPrice):
+						priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
+						priceBedroom3.append(float(priceValue))
+						currentArea=row['Internal Area']
+						areaBedroom3.append(float(currentArea))
 
-# priceBedroom1=np.array(priceBedroom1)
-medianBedroom1=np.median(priceBedroom1)
-averageBedroom1=np.average(priceBedroom1)
-perSqm1=np.median(np.divide(priceBedroom1, areaBedroom1))
+	# priceBedroom1=np.array(priceBedroom1)
+	medianBedroom1=np.median(priceBedroom1)
+	averageBedroom1=np.average(priceBedroom1)
+	perSqm1=np.median(np.divide(priceBedroom1, areaBedroom1))
 
-medianBedroom2=np.median(priceBedroom2)
-averageBedroom2=np.average(priceBedroom2)
-perSqm2=np.median(np.divide(priceBedroom2, areaBedroom2))
+	medianBedroom2=np.median(priceBedroom2)
+	averageBedroom2=np.average(priceBedroom2)
+	perSqm2=np.median(np.divide(priceBedroom2, areaBedroom2))
 
-medianBedroom3=np.median(priceBedroom3)
-averageBedroom3=np.average(priceBedroom3)
-perSqm3=np.median(np.divide(priceBedroom3, areaBedroom3))
+	medianBedroom3=np.median(priceBedroom3)
+	averageBedroom3=np.average(priceBedroom3)
+	perSqm3=np.median(np.divide(priceBedroom3, areaBedroom3))
 
-print 'per unit'
-print medianBedroom1
-print medianBedroom2
-print medianBedroom3
+	print 'per unit'
+	print medianBedroom1
+	print medianBedroom2
+	print medianBedroom3
 
-print '\nper sqm'
-print perSqm1
-print perSqm2
-print perSqm3
+	print '\nper sqm'
+	print perSqm1
+	print perSqm2
+	print perSqm3
 
+
+if __name__=='__main__':
+
+	#enquirySub="ASHFIELD"
+	enquirySub=sys.argv[1]
+	enquiryTable=sys.argv[2]
+	print enquiryTable+'\n'  ###'../suburb_profiling/history_data/2016_01_20_lots.csv'
+	suburbExtraction(enquirySub, enquiryTable)
 
 		# count=count+1
 
