e6de95a236206c5f3025cb18f8b3520371d4429f
JuanLu <juanlu@JuanLus-MacBook-Pro.local>
2016-01-25 17:58:15 +1100

    add new command interface, and the property availabiltiy filter

--- intervalPrice.py
+++ intervalPrice.py
@@ -8,11 +8,15 @@ import csv
 from suburbExtraction import * 
 import os.path
 import sys
+#import collections
+import numpy as np
+#import optparse
+import argparse
 
 
 fileDir='/Users/juanlu/Workspace/suburb_profiling/history_data/'
 
-def intervalPrice(enquirySub, startDate, interval):
+def intervalPrice(enquirySub, startDate, interval, statusCheck):
 	##extract the year, month and date
 	startPoint=dparser.parse(startDate,fuzzy=True)
 
@@ -21,10 +25,13 @@ def intervalPrice(enquirySub, startDate, interval):
 	#get the date list
 	#print startPoint
 	daysList=[startPoint-datetime.timedelta(days=x) for x in range(0, interval)]
-	#print dateList
+	#print daysList
 
+	#import pdb; pdb.set_trace()
 	priceUnitDic={}
 	priceSqmDic={}
+	priceChangeDic={}
+	
 	for days in daysList:
 		# currentDay=days.timetuple()
 		# year=currentDay[0]
@@ -47,13 +54,48 @@ def intervalPrice(enquirySub, startDate, interval):
 		if os.path.isfile(fileFullPath):
 			#print fileFullPath
 			#print currentDate
-			currentPrice=suburbExtraction(enquirySub, fileFullPath)
+
+			if statusCheck==True:
+				statusCheck='a'
+			else:
+				statusCheck=None
+			currentPrice=suburbExtraction(enquirySub, fileFullPath, statusCheck)
 			if currentPrice!=None:
 				#print currentPrice
 				priceUnitDic[days]=currentPrice['price per unit']
 				priceSqmDic[days]=currentPrice['price per sqm']
+				# if ind!=0: #calculate the rate of change
+				# 	dayAfter=days+datetime.timedelta(days=1)
+				# 	priceAfter=priceUnitDic[dayAfter]
+				# 	priceCurrentDay=currentPrice['price per unit']
+				# 	priceChange=np.divide((np.array(priceAfter)-np.array(priceCurrentDay)), np.array(priceCurrentDay) )
+				#	priceChangeDic[dayAfter]=priceChange
+					# print np.array(priceAfter)
+					# print np.array(priceCurrentDay)
+					# print priceChange
+					#import pdb; pdb.set_trace()
 				#print priceUnitDic,priceSqmDic
-	return [priceUnitDic, priceSqmDic]			
+		#ind=ind+1
+	
+	###calculate the rate of change of price per unit
+	
+	#sortedPriceUnitDic=collections.OrderedDict(sorted(priceUnitDic.items(),reverse=True))
+	ind=0
+	for key, value in sorted(priceUnitDic.iteritems(), reverse=True):
+
+		if ind!=0:
+			#dayAfter=key+datetime.timedelta(days=1)
+			priceCurrent=value
+			#priceAfter=priceUnitDic[dayAfter]
+			priceChange=np.divide(np.array(priceAfter)-np.array(priceCurrent), np.array(priceCurrent))
+			priceChangeDic[dayAfter]=priceChange
+		dayAfter=key
+		priceAfter=value
+		ind=ind+1
+
+	priceChangeDic[dayAfter]=[0,0,0]
+	#import pdb; pdb.set_trace()
+	return [priceUnitDic, priceSqmDic, priceChangeDic]			
 
     # if (os.path.isfile(fileFullPath)==Ture):
     # 	print currentDate
@@ -67,26 +109,37 @@ def intervalPrice(enquirySub, startDate, interval):
 	# f.close()
 	#break
 def priceDic2csv(priceDic):
-  	 pricePerUnit=priceDic[0]
-  	 pricePerSqm=priceDic[1]
+	"""put the data in priceDic to result.csv 
+	"""
+  	pricePerUnit=priceDic[0]
+  	pricePerSqm=priceDic[1]
+  	pricePerChange=priceDic[2]
 
   	 #print pricePerUnit
   	 #print pricePerSqm 
 
-  	 outputFile=open('result.csv', 'w')
-  	 writer=csv.writer(outputFile)
-  	 writer.writerow(["price per unit"])
-  	 writer.writerow([ None,'1 bedroom', '2 bedrooms','3 bedrooms' ])
-  	 for key, value in sorted(pricePerUnit.iteritems(), reverse=True):
-  	 	writer.writerow([key.strftime("%y/%m/%d"), '${:,.2f}'.format(value[0]), '${:,.2f}'.format(value[1]), '${:,.2f}'.format(value[2])])
+  	outputFile=open('output.csv', 'w')
+  	writer=csv.writer(outputFile)
+	writer.writerow(["price per unit"])
+	writer.writerow([ None,'1 bedroom', '2 bedrooms','3 bedrooms' ])
+	for key, value in sorted(pricePerUnit.iteritems(), reverse=True):
+		writer.writerow([key.strftime("%y/%m/%d"), '${:,.2f}'.format(value[0]), '${:,.2f}'.format(value[1]), '${:,.2f}'.format(value[2])])
 
-  	 writer.writerow([])	
-  	 writer.writerow(["price per sqm"])
-  	 writer.writerow([ None,'1 bedroom', '2 bedrooms','3 bedrooms' ])
-  	 for key, value in sorted(pricePerSqm.iteritems(), reverse=True):
-  	 	writer.writerow([key.strftime("%y/%m/%d"), '${:,.2f}'.format(value[0]), '${:,.2f}'.format(value[1]), '${:,.2f}'.format(value[2])])
+	writer.writerow([])	
+	writer.writerow(["price per sqm"])
+	writer.writerow([ None,'1 bedroom', '2 bedrooms','3 bedrooms' ])
+	for key, value in sorted(pricePerSqm.iteritems(), reverse=True):
+		writer.writerow([key.strftime("%y/%m/%d"), '${:,.2f}'.format(value[0]), '${:,.2f}'.format(value[1]), '${:,.2f}'.format(value[2])])
 
-  	 outputFile.close()
+	writer.writerow([])
+	writer.writerow(["rate of change"])
+	writer.writerow([ None,'1 bedroom', '2 bedrooms','3 bedrooms' ])
+	for key, value in sorted(pricePerChange.iteritems(), reverse=True):
+		writer.writerow([key.strftime("%y/%m/%d"), '{:,.2f}%'.format(value[0]*100), '{:,.2f}%'.format(value[1]*100), '{:,.2f}%'.format(value[2]*100)])
+
+
+
+	outputFile.close()
 
 
 if __name__=='__main__':
@@ -95,9 +148,36 @@ if __name__=='__main__':
 	# enquirySub="EPPING"
 	# interval=10
 	#enquirySub="ASHFIELD"
-	enquirySub=sys.argv[1]
-	startDate=sys.argv[2]
-	interval=sys.argv[3]
+	
 
-	result=intervalPrice(enquirySub, startDate, int(interval))
+	# parser=optparse.OptionParser()
+
+	# parser.add_option("-a", "--statusAvailable", help="only consider the available properties",dest="statusCheck", action="store_true", default=False)
+
+	# (options, args)=parser.parse_args(sys.argv)
+
+	# enquirySub=sys.argv[1]
+	# startDate=sys.argv[2]
+	# interval=sys.argv[3]
+
+	parserArg=argparse.ArgumentParser()
+	
+
+	parserArg.add_argument("enquirySub")
+	parserArg.add_argument("startDate")
+	parserArg.add_argument("interval", type=int)
+	parserArg.add_argument("-a", "--statusAvailable", help="only consider the available properties",dest="statusCheck", action="store_true", default=False)
+
+
+	args=parserArg.parse_args( )
+
+	#import pdb; pdb.set_trace()
+	result=intervalPrice(args.enquirySub, args.startDate, args.interval, args.statusCheck)
 	priceDic2csv(result)
+
+
+
+
+
+
+
--- suburbExtraction.py
+++ suburbExtraction.py
@@ -10,7 +10,7 @@ import sys
 calculate the median price per unit and per square. The result is shown based on the number of bedrooms from 1 to 3. 
 """
 
-def suburbExtraction(enquirySub, enquiryTable):
+def suburbExtraction(enquirySub, enquiryTable, statusCheck=None):
 
 	#filteredRow=[]
 	bedroom1=[]
@@ -24,86 +24,128 @@ def suburbExtraction(enquirySub, enquiryTable):
 	areaBedroom2=[]
 	areaBedroom3=[]
 	
-	#count=0
-	
+	count=0
+	#import pdb; pdb.set_trace()
+	# if statusCheck=='a':
+	# 	check=1     #only check the available property
+	# else
+	# 	check=0
+
 	with open(enquiryTable, 'rb') as csvfile:
 		#reader=csv.reader(csvfile)
 		reader=csv.DictReader(csvfile)
 		next(reader, None)
-		for row in reader:
-			# if count==0:	#the header of the table
-			# 	print row[0]
-			# 	newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
-			# 	filteredRow.append(newRow)
-			# 	count=count+1
-			projectName=row['Project']
-			destinateStr=enquirySub
-			po=projectName.find(destinateStr)
-			
-			if po>=0:
-				#newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
-				#filteredRow.append(newRow)
+		try: 
+			for row in reader:
+				# if count==0:	#the header of the table
+				# 	print row[0]
+				# 	newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
+				# 	filteredRow.append(newRow)
+				# 	count=count+1
+				if row!=None:
+					
+					projectName=row['Project']
+					destinateStr=enquirySub
+					po=projectName.find(destinateStr)
+					
 
-				Bed1=["1","1+S"]
-				Bed2=["2", "2+S"]
-				Bed3=["3", "3+S"]	
+					if po>=0:
+						#newRow=[row[SUBURBCOL],row[INTERNALAREACOL], row[BEDROOMCOL], row[PRICECOL], row[DATECOL]]
+						#filteredRow.append(newRow)
 
-				if row['Bedrooms'] in  Bed1: 
-					currentPrice=row['Price']
-					if len(currentPrice) <= 0:
-						continue
-					priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
+						Bed1=["1","1+S"]
+						Bed2=["2", "2+S"]
+						Bed3=["3", "3+S"]	
+						currentStatus=row['Sale Status']
+						#print currentStatus, statusCheck
 
-					# priceValue= currentPrice[1:len(currentPrice)]
-					# print ast.literal_eval(priceValue)
-					priceBedroom1.append(float(priceValue))
+						### check if the data should be added to the list 
+						if (statusCheck=='a' and currentStatus=='Available'):  ##when the required status is availbe
+							check=1
+						elif statusCheck== None:  ##when there is no requirement on the status of properties
+							check=1
+						else:
+							check=0
 
-					#calculate the area
-					currentArea=row['Internal Area']
-					areaBedroom1.append(float(currentArea))
+						#import pdb; pdb.set_trace()
+						#print check
 
-				if row['Bedrooms'] in  Bed2: 
-					currentPrice=row['Price']
-					if bool(currentPrice):
-						#print currentPrice
-						priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
+						if check==1:
+							count=count+1
+							#print count
+							if row['Bedrooms'] in  Bed1: 
+								currentPrice=row['Price']
+								if len(currentPrice) <= 0:
+									continue
+								priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
 
-						priceBedroom2.append(float(priceValue))
+								# priceValue= currentPrice[1:len(currentPrice)]
+								# print ast.literal_eval(priceValue)
+								priceBedroom1.append(float(priceValue))
 
-						currentArea=row['Internal Area']
-						areaBedroom2.append(float(currentArea))
+								#calculate the area
+								currentArea=row['Internal Area']
+								areaBedroom1.append(float(currentArea))
+
+							if row['Bedrooms'] in  Bed2: 
+								currentPrice=row['Price']
+								if bool(currentPrice):
+									#print currentPrice
+									priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
+
+									priceBedroom2.append(float(priceValue))
+
+									currentArea=row['Internal Area']
+									areaBedroom2.append(float(currentArea))
 
 
-				if row['Bedrooms'] in  Bed3: 
-					currentPrice=row['Price']
-					if bool(currentPrice):
-						priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
-						priceBedroom3.append(float(priceValue))
-						currentArea=row['Internal Area']
-						areaBedroom3.append(float(currentArea))
+							if row['Bedrooms'] in  Bed3: 
+								currentPrice=row['Price']
+								if bool(currentPrice):
+									priceValue= Decimal(sub(r'[^\d.]', '', currentPrice))
+									priceBedroom3.append(float(priceValue))
+									currentArea=row['Internal Area']
+									areaBedroom3.append(float(currentArea))
+						else:
+							continue
+		except csv.Error as e:
+			sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
 
 	# priceBedroom1=np.array(priceBedroom1)
-	medianBedroom1=np.median(priceBedroom1)
-	averageBedroom1=np.average(priceBedroom1)
-	perSqm1=np.median(np.divide(priceBedroom1, areaBedroom1))
+	#print count
+	if count!=0:
+		medianBedroom1=np.median(priceBedroom1)
+		#averageBedroom1=np.average(priceBedroom1)
+		perSqm1=np.median(np.divide(priceBedroom1, areaBedroom1))
+		
 
-	medianBedroom2=np.median(priceBedroom2)
-	averageBedroom2=np.average(priceBedroom2)
-	perSqm2=np.median(np.divide(priceBedroom2, areaBedroom2))
+		medianBedroom2=np.median(priceBedroom2)
+		#averageBedroom2=np.average(priceBedroom2)
+		perSqm2=np.median(np.divide(priceBedroom2, areaBedroom2))
 
-	medianBedroom3=np.median(priceBedroom3)
-	averageBedroom3=np.average(priceBedroom3)
-	perSqm3=np.median(np.divide(priceBedroom3, areaBedroom3))
+		medianBedroom3=np.median(priceBedroom3)
+		#averageBedroom3=np.average(priceBedroom3)
+		perSqm3=np.median(np.divide(priceBedroom3, areaBedroom3))
 
-	print 'per unit'
-	print medianBedroom1
-	print medianBedroom2
-	print medianBedroom3
+		# print 'per unit'
+		# print medianBedroom1
+		# print medianBedroom2
+		# print medianBedroom3
 
-	print '\nper sqm'
-	print perSqm1
-	print perSqm2
-	print perSqm3
+		# print '\nper sqm'
+		# print perSqm1
+		# print perSqm2
+		# print perSqm3
+		# print '\n'
+
+		return {'price per unit':[medianBedroom1,medianBedroom2,medianBedroom3], 'price per sqm':[perSqm1,perSqm2,perSqm3]}
+	else:
+		#print "no data\n"
+		return None
+
+
+
+
 
 
 if __name__=='__main__':
@@ -111,8 +153,9 @@ if __name__=='__main__':
 	#enquirySub="ASHFIELD"
 	enquirySub=sys.argv[1]
 	enquiryTable=sys.argv[2]
+	statusCheck=sys.argv[3]
 	print enquiryTable+'\n'  ###'../suburb_profiling/history_data/2016_01_20_lots.csv'
-	suburbExtraction(enquirySub, enquiryTable)
+	suburbExtraction(enquirySub, enquiryTable, statusCheck)
 
 		# count=count+1
 
